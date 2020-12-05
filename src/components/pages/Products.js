0fcbfa174c8e9fc94aa384a7e5e084090aa829ab
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-05 18:54:06 +0900

    Add Other Pages

---
+++ src/components/pages/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Products() {
+  return (
+    <>
+    <img src="/images/img-2.jpg"/>
+      <h1 className="products">PRODUCTS</h1>
+    </>
+  );
+}

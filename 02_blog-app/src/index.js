b315a8b081eba64f21f94eeb96e9875c41911b9a
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-09 17:50:49 +0900

    Add content into ApprovalCard

--- 02_blog-app/src/index.js
+++ 02_blog-app/src/index.js
@@ -7,8 +7,10 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard/>
-            <CommentDetail personWhowritethis="Kenji" timeAgo="Today at 2:00" content="Nice blog Post"/>
+            <ApprovalCard>
+                <CommentDetail personWhowritethis="Kenji" timeAgo="Today at 2:00" content="Nice blog Post"/>
+            </ApprovalCard>
+            
             <CommentDetail personWhowritethis="Naoki" timeAgo="Today at 4:00" content="I like the subjects"/>
             <CommentDetail personWhowritethis="Tatuya" timeAgo="Today at 6:00" content="What's do you think ?"/>
         </div>

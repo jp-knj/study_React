9293fbc91b87b0ec8c920954db35a4bf2db1cc4f
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-10 01:56:39 +0900

    Add js files

--- 03_seasons-app/src/index.js
+++ 03_seasons-app/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    return <div>Hi There!</div>
-};
+class App extends React.Component {
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector("#root")
-);
+    constructor(props){
+        super(props);
+
+        // This is the only time we do direct assignment.
+        // to this.state
+        this.state = { lat: null };
+    }
+
+    // React says we have to difine render
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+
+                // Call setState
+                this.setState({ lat: position.coords.latitude });
+
+                // did not
+                // this.state.late = position.coords.latitude
+            },
+            err => console.log(err)
+        );
+
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));

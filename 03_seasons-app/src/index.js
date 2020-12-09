3e5f20afc5dbc653086e068b676fd52af7ad6b4f
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-09 18:49:00 +0900

    Initail project

---
+++ 03_seasons-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+const App = () => {
+    return <div>Hi There!</div>
+};
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+);

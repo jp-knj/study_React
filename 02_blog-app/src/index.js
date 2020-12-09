22834eca3fd2a3f99136cee84a8d9b52afbb510a
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-09 18:01:09 +0900

    Updated Marksup card

--- 02_blog-app/src/index.js
+++ 02_blog-app/src/index.js
@@ -8,11 +8,20 @@ const App = () => {
     return (
         <div className="ui container comments">
             <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure you want to do this !
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail personWhowritethis="Kenji" timeAgo="Today at 2:00" content="Nice blog Post"/>
             </ApprovalCard>
-            
-            <CommentDetail personWhowritethis="Naoki" timeAgo="Today at 4:00" content="I like the subjects"/>
-            <CommentDetail personWhowritethis="Tatuya" timeAgo="Today at 6:00" content="What's do you think ?"/>
+            <ApprovalCard>
+                <CommentDetail personWhowritethis="Naoki" timeAgo="Today at 4:00" content="I like the subjects"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail personWhowritethis="Tatuya" timeAgo="Today at 6:00" content="What's do you think ?"/>
+            </ApprovalCard>
         </div>
     );
 };

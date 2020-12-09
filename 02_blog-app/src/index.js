69412c62210fd25b13f7c897028497a5aa1c84fb
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-09 14:44:41 +0900

    Add Marksup to display post

--- 02_blog-app/src/index.js
+++ 02_blog-app/src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div> Hi! there!</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src='https://source.unsplash.com/random' />
+                </a>
+            </div>
+            <div className="content">
+                <a href="/" className="author">
+                    Kenji
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6:00PM</span>
+                </div>
+                <div className="text">Nice Blog Post!</div>
+            </div>
+        </div>
+    );
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));

fa3b321d8325ee897ca40c3cf4904dd1cf9d98d4
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-12 01:17:00 +0900

    updated icon to display properlly

--- 03_seasons-app/src/SeasonDisplay.js
+++ 03_seasons-app/src/SeasonDisplay.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './SeasonDisplay.css'
 
 const seasonConfig = {
   summer: {
     text: 'Lets hits the beach!',
-    iconName: 'Sun'
+    iconName: 'sun'
   },
   winter: {
     text: 'Burr, it is chilly',
@@ -25,10 +26,10 @@ const SeasonDisplay = props => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${iconName} icon`}/>
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}/>
         <h1>{text}</h1>
-        <i className={`${iconName} icon`}/>
+        <i className={`icon-right massive ${iconName} icon`}/>
     </div>
     
   );

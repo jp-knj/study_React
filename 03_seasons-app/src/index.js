fd3a5bedc89b2d32585651d41a24a359bdfe14cd
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-11 23:56:57 +0900

    Add text to display season

--- 03_seasons-app/src/index.js
+++ 03_seasons-app/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // THIS IS THE ONLY TIME we do direct assignment
-    // to this.state
-    this.state = { lat: null, errorMessage: '' };
-  }
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
@@ -17,18 +12,14 @@ class App extends React.Component {
     );
   }
 
-  componentDidUpdate(){
-    console.log('My component was just updated - it rerendered');
-  }
-
   // React says we have to define render!!
   render() {
     if (this.state.errorMessage && !this.state.lat){
-      return <div>Error :{ this.state.errorMessage}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     }
 
     if (!this.state.errorMessage && this.state.lat){
-      return <div>Latitude :{ this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     }
       return <div>Loading!</div>
   }

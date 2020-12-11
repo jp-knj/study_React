e352a02569b76aad4341d72d3d13478cdd31fb94
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-11 22:43:51 +0900

    Add some lifecycle methods

--- 03_seasons-app/src/index.js
+++ 03_seasons-app/src/index.js
@@ -2,31 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = { lat: null, errorMessage: '' };
 
-        // This is the only time we do direct assignment.
-        // to this.state
-        this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+
+        // Called setstate
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidMount(){
+    console.log('My component was rendered to the screen');
+  }
+
+  componentDidUpdate(){
+    console.log('My component was just updated - it rerendered');
+  }
+  
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat){
+      return <div>Error :{ this.state.errorMessage}</div>
     }
 
-    // React says we have to difine render
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-
-                // Call setState
-                this.setState({ lat: position.coords.latitude });
-
-                // did not
-                // this.state.late = position.coords.latitude
-            },
-            err => console.log(err)
-        );
-
-        return <div>Latitude: {this.state.lat}</div>;
+    if (!this.state.errorMessage && this.state.lat){
+      return <div>Latitude :{ this.state.lat}</div>
     }
+      return <div>Loading!</div>
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));

ca6da726e72ca4f3c697df649c9df1b7e0ea4baa
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-19 20:58:07 +0900

    Add some script

--- 04_pictures-app/src/components/App.js
+++ 04_pictures-app/src/components/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar/>
-        </div>
-    );
-};
+
+class App extends React.Componet {
+    async onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID Fi9_st6VmbMSu3NruyH97uoYOI7Iz1GN33xOvaOKKDc'
+            }
+        });
+        console.log(response.data.results);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+}
 
 export default App;

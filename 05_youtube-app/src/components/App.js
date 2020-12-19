f1804dc3a56860cf066ad489d229997060065bd5
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-20 05:53:22 +0900

    Deeply Nested Callbacks

--- 05_youtube-app/src/components/App.js
+++ 05_youtube-app/src/components/App.js
@@ -3,24 +3,34 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
+const KEY = 'AIzaSyCWS4dsK2LSasr_NzzWDhiu3Bs_4AZE6F0';
+
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit =  async term  => {
     const response =  await youtube.get('/search', {
       params: {
-        q: term
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY,
       }
     });
 
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    console.log('From the App!', video);
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={ this.state.videos}/>
+        <VideoList onVideSelect={this.onVideoSelect} videos={ this.state.videos}/>
       </div>
     );
   }

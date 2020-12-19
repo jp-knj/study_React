0815ac1d1ba944dee3ac937a56138701074f34a0
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-19 22:16:59 +0900

    Add some messages

--- 04_pictures-app/src/components/ImageList.js
+++ 04_pictures-app/src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const ImageList = props => {
-  const image = props.images.map((image) => {
-    return <img src={ image.urls.regular} />
+  const image = props.images.map(( description, id, urls ) => {
+    return <img alt={description} key={id} src={ urls.regular} />
   });
 
   return <div>{images}</div>

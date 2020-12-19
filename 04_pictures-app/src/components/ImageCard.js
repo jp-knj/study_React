e5f31004dfc5ba5fde47268ee8f1d7978a2e459e
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-19 22:46:13 +0900

    Add ImageCard

---
+++ 04_pictures-app/src/components/ImageCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  render() {
+
+    const { description, urls } = this.props.image;
+    return (
+      <div>
+        <img
+          alt={this.props.image.description}
+          src={this.props.image.urls.regular}
+        />
+      </div>
+    )
+  }
+}

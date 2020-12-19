4c0adefd4676d3b1f3be2fbdb7cf070e98e56213
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-20 05:15:31 +0900

    Rendering Video Thumbnails

--- 05_youtube-app/src/components/VideoItem.js
+++ 05_youtube-app/src/components/VideoItem.js
@@ -1,9 +1,13 @@
 import { React } from "react";
 
-const VideoItem = props => {
+const VideoItem = ({video}) => {
   return (
-    <div>Video Item</div>
-  )
+
+    <div>
+      <img src={video.snippet.thumbnails.medium.url} />
+      {video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;

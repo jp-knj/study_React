b6cdf7db574899c8c8cf12286e85a9165aea558f
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-19 23:19:16 +0900

    Finished image card styling

--- 04_pictures-app/src/components/ImageCard.js
+++ 04_pictures-app/src/components/ImageCard.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
 
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 150);
+
+    this.setState({ spans });
+  };
+
+  render() {
     const { description, urls } = this.props.image;
     return (
       <div>
         <img
+          ref={this.imageRef}
           alt={this.props.image.description}
           src={this.props.image.urls.regular}
         />

f3447f50b36ec857f747bdb9958fc80fc002dbd8
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-12-21 14:58:49 +0900

    Hiding the option list

--- 06_widgets-app/src/App.js
+++ 06_widgets-app/src/App.js
@@ -1,42 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
 const items = [
   {
-    title: 'What is React ?',
-    content: 'React is a Front end JavaScript framework'
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
   },
   {
-    title: 'Why use React ?',
-    content: 'React is a favorite JavaScript Library amang Enginners'
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
   },
   {
-    title: 'How do you use React ?',
-    content: 'You use React by creating components'
-  }
-]
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
 
 const options = [
   {
     label: 'The Color Red',
-    value: 'red'
+    value: 'red',
   },
   {
     label: 'The Color Green',
-    value: 'Green'
+    value: 'green',
   },
   {
     label: 'A Shade of Blue',
-    value: 'blue'
+    value: 'blue',
   },
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Dropdown options={options}/>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };

76718ef90464c2cb632a4dfae5c0be48d54be684
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-07 00:27:44 +0900

    :+1: Add Hero section and Install node-sass and destyle.css
    t

---
+++ 08_website/src/components/hero.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <>
+      <div class="shadow-title parallax-top-shadow">Consortium</div>
+      <div class="section full-height">
+        <div class="section-title-wrap">
+          <div class="container">
+            <div class="row">
+              <div class="col-12 text-center parallax-fade-top">
+                <p>we do magic</p>
+                <h1>成熟社会<br />Consortium</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default Hero;

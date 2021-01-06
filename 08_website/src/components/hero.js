850c6d9316188a16f79e754c5fe1db202333924e
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-07 02:36:24 +0900

    :+1: created Hero section
    t

--- 08_website/src/components/hero.js
+++ 08_website/src/components/hero.js
@@ -4,18 +4,18 @@ export const Hero = () => {
   return (
     <>
       <div class="shadow-title parallax-top-shadow">Consortium</div>
-      <div class="section full-height">
+      <section class="hero">
         <div class="section-title-wrap">
           <div class="container">
             <div class="row">
               <div class="col-12 text-center parallax-fade-top">
                 <p>we do magic</p>
-                <h1>成熟社会<br />Consortium</h1>
+                <h1>成熟社会<br /><span>Consortium</span></h1>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   )
 }

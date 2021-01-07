17e4c2f9dcb7d0e30b1e2935b57f6f0a06e92de2
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-08 00:37:54 +0900

    :sparkles: Add keyframes
    t

--- 11_framermotion-website/src/Animation/Example.js
+++ 11_framermotion-website/src/Animation/Example.js
@@ -1,4 +1,22 @@
 import * as React from "react";
 import { motion } from "framer-motion";
 
-export const Example = () => <motion.div animate={{ scale: 2 }} />;
+export const Example = () => (
+  <>
+  {/* <motion.div
+    animate={{ scale: 2 }}
+    transition={{ duration: 1 }}
+  /> */}
+
+  <motion.div
+    animate={{
+      scale: [1, 2, 2, 1, 1],
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    }}
+    transition={{
+      duration: 5
+    }}
+    />
+  </>
+);

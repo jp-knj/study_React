79250df0ddca6abde433d932022d71ac13d07c38
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-07 11:01:54 +0900

    :+1: Update to markup Slider section
    t

--- 08_website/src/components/slider.js
+++ 08_website/src/components/slider.js
@@ -22,41 +22,40 @@ export const slider = () => {
           <li>
             <div class="img-hero-background">
               <img src="http://www.ivang-design.com/svg-load/parallax/light.jpg" alt=""/>
+            </div>
+            <div class="hero-number-back">01</div>
+            <div class="hero-number">01</div>
+            <div class="hero-number-fixed">04</div>
+            <div class="case-study-title">graphic design, interaction</div>
+          </li>
+          <li>
+            <div class="img-hero-background">
+              <img src="http://www.ivang-design.com/svg-load/parallax/flare.jpg" alt=""/>
+            </div>
+            <div class="hero-number-back">02</div>
+            <div class="hero-number">02</div>
+            <div class="case-study-title">advertising, art direction</div>
+          </li>
+          <li>
+            <div class="img-hero-background">
+              <img src="http://www.ivang-design.com/svg-load/parallax/nature.jpg" alt=""/>
+            </div>
+            <div class="hero-number-back">03</div>
+            <div class="hero-number">03</div>
+            <div class="case-study-title">photography, retouching</div>
+          </li>
+          <li>
+            <div class="img-hero-background">
+              <img src="http://www.ivang-design.com/svg-load/parallax/fire.jpg" alt=""/>
+            </div>
+            <div class="hero-number-back">04</div>
+            <div class="hero-number">04</div>
+            <div class="case-study-title">photography, advertising</div>
+          </li>
+        </ul>
       </div>
-              <div class="hero-number-back">01</div>
-              <div class="hero-number">01</div>
-              <div class="hero-number-fixed">04</div>
-              <div class="case-study-title">graphic design, interaction</div>
-    </li>
-            <li>
-              <div class="img-hero-background">
-                <img src="http://www.ivang-design.com/svg-load/parallax/flare.jpg" alt=""/>
-      </div>
-                <div class="hero-number-back">02</div>
-                <div class="hero-number">02</div>
-                <div class="case-study-title">advertising, art direction</div>
-    </li>
-              <li>
-                <div class="img-hero-background">
-                  <img src="http://www.ivang-design.com/svg-load/parallax/nature.jpg" alt=""/>
-      </div>
-                  <div class="hero-number-back">03</div>
-                  <div class="hero-number">03</div>
-                  <div class="case-study-title">photography, retouching</div>
-    </li>
-                <li>
-                  <div class="img-hero-background">
-                    <img src="http://www.ivang-design.com/svg-load/parallax/fire.jpg" alt=""/>
-      </div>
-                    <div class="hero-number-back">04</div>
-                    <div class="hero-number">04</div>
-                    <div class="case-study-title">photography, advertising</div>
-    </li>
-  </ul>
-</div>
-
     </div>
   )
 }
 
-export default slider;
+export default Slider;

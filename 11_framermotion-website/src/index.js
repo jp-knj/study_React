3ff207aace86e658ad0e8ef8b150623f02a20b12
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-08 01:06:33 +0900

    :sparkles: Learn Drag
    t

--- 11_framermotion-website/src/index.js
+++ 11_framermotion-website/src/index.js
@@ -1,9 +1,21 @@
 import * as React from "react";
+import { useState } from "react";
 import { render } from "react-dom";
-import { Example } from "./Variants/Example";
+import { Example } from "./Drag/Example";
+import { Refresh } from "./Drag/Refrash";
 
-import "./Variants/styles.css";
+import "./Drag/styles.css";
 
-const App = () => <Example />;
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Refresh onClick={() => setCount(count + 1)} />
+      <div className="example-container">
+        <Example key={count} />
+      </div>
+    </>
+  );
+};
 
 render(<App />, document.getElementById("root"));

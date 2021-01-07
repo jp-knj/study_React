14e94fe8eeb609edc3e50f3de4b316eb9a14f998
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-07 16:11:11 +0900

    :+1: Add TranslateStaggeredBars section
    t

--- 09_reactspring-website/src/Tutorial/TranslateStaggeredBars.js
+++ 09_reactspring-website/src/Tutorial/TranslateStaggeredBars.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { useTrail, animated, useSpring, useChain, config } from 'react-spring';
+
+const colors = ['red', 'green', 'blue', 'orange', 'purple', 'yellow'];
 
 export const TranslateStaggeredBars = () => {
-  return (
-    <div>
+  const [expanded, setExpanded] = useState(false);
 
+  const springRef = useRef();
+  const spring = useSpring({
+    from: { transform: `translateX(80px)` },
+    to: { transform: `translateX(0px)` },
+    ref: springRef,
+    config: config.stiff,
+    reverse: expanded,
+  });
+
+  const trailRef = useRef();
+  const trailSprings = useTrail(colors.length, {
+    from: { height: '5px' },
+    to: { height: '80px' },
+    ref: trailRef,
+    reverse: !expanded,
+  });
+
+  useChain(expanded ? [springRef, trailRef] : [trailRef, springRef]);
+
+  return (
+    <div style={{ height: '500px' }}>
+      <animated.div
+        style={{
+          ...spring,
+          height: '100px',
+          display: 'inline-flex',
+          alignItems: 'flex-end',
+          marginBottom: '15px',
+        }}
+      >
+        {trailSprings.map((trailSpring, index) => (
+          <animated.div
+            key={colors[index]}
+            style={{
+              ...trailSpring,
+              width: '20px',
+              marginRight: '10px',
+              transformOrigin: 'bottom',
+              backgroundColor: colors[index],
+            }}
+          />
+        ))}
+      </animated.div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setExpanded(prevState => !prevState);
+          }}
+        >
+          Click to Animate
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TranslateStaggeredBars;

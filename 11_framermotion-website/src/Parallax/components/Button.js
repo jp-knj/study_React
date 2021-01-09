a29452bd8e37c5c2bebdd9981a0d6251010ca61e
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-10 01:18:45 +0900

    :+1: Add icons
    t

--- 11_framermotion-website/src/Parallax/components/Button.js
+++ 11_framermotion-website/src/Parallax/components/Button.js
@@ -1,21 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
 import { motion } from "framer-motion";
+import { FiArrowRight } from "react-icons/fi";
 
 export const Button = () => {
   return (
-    <ButtonStyle whileHover={{ scale: 1.1 }}>
-      Read More</ButtonStyle>
+    <FiArrowRight size={48}/>
   )
 };
-
-const ButtonStyle = styled(motion.button)`
-  width: 130px;
-  height: 50px;
-	margin: 0 4px;
-	border-radius: 15px;
-	border: 2px solid black;
-  background: black;
-  color: white;
-  text-align: center;
-`

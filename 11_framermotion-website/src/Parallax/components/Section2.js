f6aa2f666842eda7982262ab2da3a7a643db89da
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-11 06:15:44 +0900

    :+1: Update Section2
    t

--- 11_framermotion-website/src/Parallax/components/Section2.js
+++ 11_framermotion-website/src/Parallax/components/Section2.js
@@ -9,8 +9,13 @@ export const Section2 = () => {
         <Container>
           <H6>活動報告について</H6>
           <H1>Activity</H1>
-          <p>こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。</p>
-          <Card/>
+          <P>地域特性を最大限に活かす知恵と工夫をこらし<br />
+          新しく構想し、生み出していく「創造」の時代に<br />
+          立ち向かわなければなりません</P>
+          <Inner>
+            <Card />
+            <Card />
+          </Inner>
         </Container>
       </Section>
     </>
@@ -29,10 +34,18 @@ const Section = styled.section`
 `;
 
 const Container = styled.div`
+ height: 900px;
+`
+
+const Inner = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `
 
 const H1 = styled.h1`
-  margin-bottom: 20px;
+  margin-left: 20px;
   font-weight: 800;
   font-size: 4rem;
   color: white;
@@ -40,14 +53,24 @@ const H1 = styled.h1`
 `
 
 const H6 = styled.h6`
+  margin-left: -5px;
+  padding: 5px;
+  text-align: center;
   font-weight: 500;
   letter-spacing: 3px;
   font-size: 1rem;
-  margin-bottom: -5px;
+  height: 170px;
   color: white;
   -webkit-text-stroke: 0.008rem black;
   background-color: black;
-  padding: 5px;
-  padding-bottom: 2px;
-  margin-bottom: 3px;
+
+`
+
+const P = styled.p`
+  margin: auto 0;
+  margin-left: 40px;
+  font-weight: 200;
+  letter-spacing: 5px;
+  font-size: 20px;
+  height: auto;
 `

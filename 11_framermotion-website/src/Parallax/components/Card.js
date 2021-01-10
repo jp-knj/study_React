32fb19af7ab12f3f5a80ec05ad57a273cb302a63
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-11 05:37:58 +0900

    :+1: Create Card
    t

--- 11_framermotion-website/src/Parallax/components/Card.js
+++ 11_framermotion-website/src/Parallax/components/Card.js
@@ -1,27 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button } from './Button'
-
+import { FiArrowRight } from "react-icons/fi";
 
 export const Card = () => {
   return (
     <Cards>
       <CardImg/>
-      <CardTitle></CardTitle>
-        <CardText></CardText>
-      <Button/>
+      <CardTitle>成長社会から成熟社会</CardTitle>
+      <CardText><FiArrowRight /></CardText>
     </Cards>
   );
 };
 
 const Cards = styled.div`
+ background-color: red;
+ height: 250px;
+ width: 400px;
 `
 
 const CardImg = styled.div`
+ background-color: green;
+ height: 250px;
+ width: 300px;
+ margin-left: 8px;
 `
 
 const CardTitle = styled.div`
+  font-weight: 500;
+  letter-spacing: 3px;
+  font-size: 18px;
 `
 
 const CardText = styled.div`
+  height: 40px;
+  width: 40px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
 `

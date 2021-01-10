b0ecb7b7c80548ec6cb4c744275727ffd0f10450
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-11 07:25:58 +0900

    :+1: Markup News
    t

--- 11_framermotion-website/src/Parallax/components/Section4.js
+++ 11_framermotion-website/src/Parallax/components/Section4.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from "styled-components";
+import styled, { ThemeConsumer } from "styled-components";
 import { Button } from './Button';
 
 export const Section4 = () => {
@@ -8,8 +8,27 @@ export const Section4 = () => {
       <Container>
         <H6>お知らせ</H6>
         <H1>News</H1>
-        <p>こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。こちらはテキストになります。</p>
-        <Button/>
+        <P>成熟社会コンソーシアムからの最新情報をお届けします。</P>
+        <NewsContainer>
+          <News>
+            <ThumbnailContainer>
+              <Thumbnail></Thumbnail>
+            </ThumbnailContainer>
+            <NewsInfo>
+              <NewsTitle></NewsTitle>
+              <NewsDate></NewsDate>
+            </NewsInfo>
+          </News>
+          <News>
+            <ThumbnailContainer>
+              <Thumbnail></Thumbnail>
+            </ThumbnailContainer>
+            <NewsInfo>
+              <NewsTitle></NewsTitle>
+              <NewsDate></NewsDate>
+            </NewsInfo>
+          </News>
+        </NewsContainer>
       </Container>
     </HeroSection>
   )
@@ -24,7 +43,7 @@ const HeroSection = styled.section`
 `
 const Container = styled.div`
   margin: 0 auto;
-  padding: 0 200px;
+  width: 1000px;
 `
 
 const H1 = styled.h1`
@@ -36,16 +55,41 @@ const H1 = styled.h1`
 `
 
 const H6 = styled.h6`
-  font-weight: 500;
-  letter-spacing: 3px;
-  font-size: 1rem;
-  margin-bottom: -5px;
-  color: white;
-  -webkit-text-stroke: 0.008rem black;
-  background-color: black;
+  width: 100px;
   padding: 5px;
   padding-bottom: 2px;
   margin-bottom: 3px;
+  font-weight: 500;
+  letter-spacing: 3px;
+  font-size: 1rem;
+  text-align: center;
+  color: white;
+  -webkit-text-stroke: 0.008rem black;
+  background-color: black;
 `
+
+const P = styled.p`
+  font-weight: 200;
+  letter-spacing: 5px;
+  font-size: 20px;
+`
+
+const News = styled.a`
+`
+
+const NewsContainer = styled.div`
+`
+
+const ThumbnailContainer = styled.div`
+`
+
+const Thumbnail = styled.div`
+`
+
+const NewsInfo = styled.div`
+`
+
+const NewsTitle = styled.p``
+const NewsDate = styled.p``
 
 export default Section4;

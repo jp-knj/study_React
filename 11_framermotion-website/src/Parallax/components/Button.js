fef2128e17e025521aee15ad441eff3067995542
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-11 03:51:50 +0900

    :+1: Add Button
    t

--- 11_framermotion-website/src/Parallax/components/Button.js
+++ 11_framermotion-website/src/Parallax/components/Button.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import { motion } from "framer-motion";
 import { FiArrowRight } from "react-icons/fi";
 
 export const Button = () => {
   return (
-    <FiArrowRight size={48}/>
+    <Btn
+      whileHover={{ width: '200px' }}
+    >
+      <FiArrowRight size={30} />
+    </Btn>
+
   )
 };
+
+const Btn = styled(motion.div)`
+  height: 50px;
+  padding-left: 10px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 50px;
+  background-color: black;
+  color: white;
+`

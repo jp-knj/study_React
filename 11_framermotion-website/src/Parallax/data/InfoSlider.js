7feb3991240aedbde589d3370f6fe4d3d5cd8bfe
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-11 14:14:17 +0900

    :sparkles: Add data of slider
    t

---
+++ 11_framermotion-website/src/Parallax/data/InfoSlider.js
@@ -0,0 +1,20 @@
+export const Info = [
+  {
+    id: 0,
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dignissimos, et repudiandae quas hic inventore quisquam quis vitae, excepturi labore laudantium laboriosam veritatis optio illo minima voluptatum pariatur iure esse, voluptatibus ut explicabo est similique molestias.",
+    name: "Kenji Tomita"
+  },
+  {
+    id: 1,
+    comment:
+      "Fugit quibusdam veniam illum iure sunt quisquam necessitatibus sequi excepturi minima harum qui aliquid, modi voluptatibus expedita quasi illo! Numquam provident, vero saepe soluta officia beatae ducimus est ut dolorem atque aperiam non suscipit voluptatem?",
+    name: "John Smith"
+  },
+  {
+    id: 2,
+    comment:
+      "Ut eius omnis rerum harum beatae ullam commodi quam quasi aperiam veniam quae esse corporis, fuga quis, animi asperiores similique nobis maiores molestias dolore ipsam. Omnis sunt iusto quasi.",
+    name: "Scarlett Light"
+  }
+];

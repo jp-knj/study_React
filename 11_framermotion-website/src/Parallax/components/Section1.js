8ab4d0d664b07f6702eda500c9f84862e8c34f45
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-11 09:48:02 +0900

    :+1: Update Section1
    t

--- 11_framermotion-website/src/Parallax/components/Section1.js
+++ 11_framermotion-website/src/Parallax/components/Section1.js
@@ -8,8 +8,11 @@ export const Section1 = () => {
         <Container>
           <H6>私たちについて</H6>
           <H1>About</H1>
-          <P>精神的豊かさや生活の<br />
-            質の向上を重視する、平和で自由な社会へめざす。</P>
+          <P>精神的豊かさや生活の</P>
+          <P>質の向上を重視する、平和で自由な社会へめざす。</P>
+          <P>我々は、<Str>高齢者特有の法的な保障</Str>について考える</P>
+          <P>高齢者にかかわる法的課題全般について専門家が</P>
+          <P>ともに検討し、情報を発信します</P>
         </Container>
       </Section>
     </>
@@ -57,4 +60,12 @@ const P = styled.p`
   font-weight: 200;
   letter-spacing: 5px;
   font-size: 20px;
+  margin-bottom: 20px;
+`
+
+const Str = styled.strong`
+  background-color: black;
+  color: white;
+  padding: 3px 5px;
+  margin: 0 3px;
 `

4ba8b4c95ee9fef2528c428235e5c9babd0882f2
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-11 17:25:08 +0900

    :+1: Add anition on Section1
    t

--- 11_framermotion-website/src/Parallax/components/Section1.js
+++ 11_framermotion-website/src/Parallax/components/Section1.js
@@ -1,18 +1,38 @@
-import React from 'react';
 import styled from 'styled-components';
+import React, { useRef, useState, useLayoutEffect } from "react";
+import { motion, useViewportScroll, useTransform, useSpring } from "framer-motion";
 
 export const Section1 = () => {
+  const { scrollY } = useViewportScroll();
+  const ref = useRef();
+  const [offsetTop, setOffsetTop] = useState(0);
+
+  useLayoutEffect(() => {
+    if (!ref.current) return null;
+    setOffsetTop(ref.current.offsetTop);
+  }, [ref]);
+
+  const opacityRange = useTransform(scrollY, [offsetTop + 1050, offsetTop + 1200], [1, 0]);
+  const yRange = useTransform(scrollY, [offsetTop + 1150, offsetTop + 1400],  [0, 100]);
+
+  const opacity = useSpring(opacityRange, { stiffness: 200, damping: 100 });
+  const y = useSpring(yRange, { stiffness: 200, damping: 100 });
+
   return (
     <>
       <Section>
         <Container>
-          <H6>私たちについて</H6>
-          <H1>About</H1>
-          <P>精神的豊かさや生活の</P>
-          <P>質の向上を重視する、平和で自由な社会へめざす。</P>
-          <P>我々は、<Str>高齢者特有の法的な保障</Str>について考える</P>
-          <P>高齢者にかかわる法的課題全般について専門家が</P>
-          <P>ともに検討し、情報を発信します</P>
+          <AboutTitle ref={ref} style={{ opacity, y }}>
+            <H6>私たちについて</H6>
+            <H1>About</H1>
+          </AboutTitle>
+          <AboutDescription style={{ opacity, y }}>
+            <P>精神的豊かさや生活の</P>
+            <P>質の向上を重視する、平和で自由な社会へめざす。</P>
+            <P>我々は、<Str>高齢者特有の法的な保障</Str>について考える</P>
+            <P>高齢者にかかわる法的課題全般について専門家が</P>
+            <P>ともに検討し、情報を発信します</P>
+          </AboutDescription>
         </Container>
       </Section>
     </>
@@ -29,8 +49,14 @@ const Section = styled.section`
 `;
 
 const Container = styled.div`
-  height: 300px;
+  height: 500px;
   width: 700px;
+`
+
+const AboutTitle = styled(motion.div)`
+`
+
+const AboutDescription = styled(motion.div)`
 `
 
 const H1 = styled.h1`

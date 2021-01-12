d689943d658c6bba15979464e82ac1371ee5bf02
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-12 16:10:10 +0900

    :+1: Update to markup Menu Section
    t

--- 11_framermotion-website/src/Parallax/components/Menu.js
+++ 11_framermotion-website/src/Parallax/components/Menu.js
@@ -4,35 +4,48 @@ import styled from 'styled-components'
 export const Menu = () => {
   return (
     <>
-      <Menucontainer>
-        <MenuInner>
-          <Menus>
-            <MenuList>
-              <Title></Title>
-              <Img></Img>
-            </MenuList>
-            <MenuList>
-              <Title></Title>
-              <Img></Img>
-            </MenuList><MenuList>
-              <Title></Title>
-              <Img></Img>
-            </MenuList><MenuList>
-              <Title></Title>
-              <Img></Img>
-            </MenuList>
-          </Menus>
-        </MenuInner>
-      </Menucontainer>
+      <MenuSection>
+        <MenuHeader>
+          <MenuClose></MenuClose>
+        </MenuHeader>
+        <Menucontainer>
+          <MenuInner>
+            <Menus>
+              <MenuList>
+                <Title>Home</Title>
+                <Img></Img>
+              </MenuList>
+              <MenuList>
+                <Title>Acitivity</Title>
+                <Img></Img>
+              </MenuList><MenuList>
+                <Title>News</Title>
+                <Img></Img>
+              </MenuList><MenuList>
+                <Title>People</Title>
+                <Img></Img>
+              </MenuList>
+            </Menus>
+          </MenuInner>
+          </Menucontainer>
+      </MenuSection>
     </>
   )
 }
 
 export default Menu;
 
-const Menucontainer = styled.div``
-const MenuInner = styled.div``
-const Menus = styled.ul``
+const MenuSection = styled.div`
+`
+const MenuHeader = styled.div``
+const MenuClose = styled.div``
+const Menucontainer = styled.div`
+`
+const MenuInner = styled.div`
+
+`
+const Menus = styled.ul`
+`
 const MenuList = styled.li``
 const Title = styled.h2``
 const Img = styled.div``

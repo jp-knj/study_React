751e1749565106e6357133f4d860f46af19ea118
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-12 10:59:19 +0900

    Learn useState
    t

--- 08_website/src/App.js
+++ 08_website/src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 
-/*
-  How do you create a state variable?
-  Syntax: const [stateVariable] = React.useState(defaultValue);
-*/
-
 function App() {
+  /*
+   The setter function is always the second destructured value.
+   The naming convention for the setter function is to be prefixed with 'set'.
+  */
+  const [language, setLanguage] = React.useState("javascript");
 
-  const [language] = React.useState('JavaScript');
-
-/*
-  We use array destructuring to declare state variable.
-  Like any variable, we declare we can name it what we like (in this case, 'language').
-*/
-
-  return <div>I am learning {language}</div>;
+  return (
+    <div>
+      <button onClick={() => setLanguage("python")}>
+        Learn Python
+      </button>
+      {/*
+        Why use an inline arrow function here instead of immediately calling it like so: onClick={setterFn()}?
+        If so, setLanguage would be called immediately and not when the button was clicked by the user.
+        */}
+      <p>I am now learning {language}</p>
+    </div>
+  );
 }
 
 export default App;
+
+/*
+ Note: whenever the setter function is called, the state updates,
+ and the App component re-renders to display the new state.
+ Whenever state is updated, the component will be re-rendered
+*/

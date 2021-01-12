28a877103e5220424670d68f74e503cef77e71d4
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-12 10:40:49 +0900

    :tada: Update new app
    t

--- 08_website/src/App.js
+++ 08_website/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Hero  from './components/hero';
-import Slider from './components/slider';
 
 function App() {
   return (
     <div className="App">
-      <Hero />
-      <Slider />
+      <h1>Hello World</h1>
     </div>
   );
 }

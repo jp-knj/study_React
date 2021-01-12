b62678be3c6db4349158ab782aa8395f400ab202
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-12 10:56:44 +0900

    State and useState
    t

--- 08_website/src/App.js
+++ 08_website/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+/*
+  How do you create a state variable?
+  Syntax: const [stateVariable] = React.useState(defaultValue);
+*/
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+
+  const [language] = React.useState('JavaScript');
+
+/*
+  We use array destructuring to declare state variable.
+  Like any variable, we declare we can name it what we like (in this case, 'language').
+*/
+
+  return <div>I am learning {language}</div>;
 }
 
 export default App;

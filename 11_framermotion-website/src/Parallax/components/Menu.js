7c8e38febcc78a6db1cb15a7a85bff3516164594
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-13 16:02:52 +0900

    :+1: Add Animation When click the menu btn
    t

--- 11_framermotion-website/src/Parallax/components/Menu.js
+++ 11_framermotion-website/src/Parallax/components/Menu.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState }from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { AiOutlineClose } from 'react-icons/ai'
 import { motion, AnimatePresence } from 'framer-motion'
 
+const transition = { duration: 0.8, ease: [0.6, -0.05, 0.01, 0.9] }
 
 export const Menu = ({ menuState, setMenuState }) => {
   return (
     <>
       <AnimatePresence>
         {menuState && (
+          <>
           <MenuSection
             initial={{ visibility: "hidden" }}
             exit={{
@@ -50,12 +52,46 @@ export const Menu = ({ menuState, setMenuState }) => {
               </MenuList>
             </Menus>
           </Menucontainer>
-        </MenuSection>
+          </MenuSection>
+          <Panels />
+          </>
         )}
       </AnimatePresence>
     </>
   )
 }
+
+const Panels = () => {
+  return (
+    <>
+      <LeftPanelBackground
+        initial={{ height: 0 }}
+        animate={{
+          height: [0, window.innerHeight, 0],
+          bottom: [null, 0, 0],
+        }}
+        exit={{
+          height: [0, window.innerHeight, 0],
+          top: [null, 0, 0],
+        }}
+        transition={{ ...transition, duration: 2, times: [0, 0.5, 1] }}
+      ></LeftPanelBackground>
+      <RightPanelBackground
+        initial={{ height: 0 }}
+        animate={{
+          height: [0, window.innerHeight, 0],
+          bottom: [0, 0, window.innerHeight],
+        }}
+        exit={{
+          height: [0, window.innerHeight, 0],
+          bottom: [null, 0, 0],
+        }}
+        transition={{ ...transition, duration: 2, times: [0, 0.5, 1] }}
+      ></RightPanelBackground>
+    </>
+  )
+}
+
 export default Menu;
 
 const MenuSection = styled(motion.div)`
@@ -121,4 +157,36 @@ const Title = styled.h2`
   font-size: 4rem;
   font-weight: 500;
 `
-const Img = styled.img``
+const LeftPanelBackground = styled(motion.div)`
+  height: 100vh;
+  width: 50vw;
+  position: absolute;
+  background-color: red;
+  z-index: 11;
+`
+
+const RightPanelBackground = styled(motion.div)`
+  height: 100vh;
+  width: 50vw;
+  position: absolute;
+  right: 0;
+  z-index: 11;
+  background-color: green;
+`
+
+// style={{
+        //   background: panelComplete ? "#e7e7de" : "#e7dee7",
+        // }}
+        // initial={{ height: 0 }}
+        // animate={{
+        //   height: [0, window.innerHeight, 0],
+        //   bottom: [0, 0, window.innerHeight],
+        // }}
+        // exit={{
+        //   height: [0, window.innerHeight, 0],
+        //   bottom: [null, 0, 0],
+        // }}
+        // transition={{ ...transition, duration: 2, times: [0, 0.5, 1] }}
+        // onAnimationComplete={() => {
+        //   setPanelComplete(!panelComplete)
+        // }}

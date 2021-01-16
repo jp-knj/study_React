07e3535bbd639f657c6ce258cf12ba9907d295ac
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-16 17:36:10 +0900

    :+1: Markup HeroSection
    t

--- 12_framer-motion-website/src/components/homePage/Hero.js
+++ 12_framer-motion-website/src/components/homePage/Hero.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export const Hero = () => {
   return (
     <>
-      <h1>Hero</h1>
+    <Section>
+        <Wrapper>
+          <Title>
+            成熟社会<br />Consotium
+          </Title>
+          <Img>
+          </Img>
+       </Wrapper>
+    </Section>
     </>
   )
 }
 
 export default Hero;
+
+const Section = styled.section`
+  height: 80vh;
+`
+
+const Wrapper = styled.div`
+  margin: 100px 0;
+  padding: 0 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Title = styled.h1`
+  text-align: center;
+  font-weight: 800;
+  font-size: 60px;
+  line-height: 70px;
+  color: white;
+  -webkit-text-stroke: 1px black;
+  margin-bottom: 50px;
+`
+const Img = styled.div`
+  height: 400px;
+  width: 500px;
+  border: 2px solid #333;
+  opacity: 0.4;
+  margin: 0;
+`

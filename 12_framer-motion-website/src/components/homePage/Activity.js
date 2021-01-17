034da29ce08f5208d5b2167bc5fff36afe57b20e
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-17 20:31:26 +0900

    :+1: Add animation
    t

--- 12_framer-motion-website/src/components/homePage/Activity.js
+++ 12_framer-motion-website/src/components/homePage/Activity.js
@@ -1,20 +1,66 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { Section, Wrapper, Title } from './Hero'
-import { P } from './About'
+import { motion ,useAnimation } from "framer-motion"
+import { useInView } from 'react-intersection-observer'
+import { Wrapper, Title, Text, P } from './About'
 
 export const Activity = () => {
+  const animation = useAnimation()
+  const [contentRef, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-300px",
+  })
+
+  useEffect(() => {
+    if (inView) { animation.start("visible") }
+  }, [animation, inView])
   return (
     <>
       <Section>
         <Wrapper>
-          <Title>
-            Activity
-          </Title>
-          <P>地域特性を最大限に活かす知恵と工夫をこらし<br />
-          新しく構想し、生み出していく「創造」の時代に<br />
-          立ち向かわなければなりません</P>
-          <ActivityContainer>
+          <Title
+            ref={contentRef}
+            animate={animation}
+            initial="hidden"
+            variants={{
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: .4, ease: [0.6, 0.05, -0.01, 0.9] },
+              },
+              hidden: { opacity: 0, y: 82 },
+            }}
+          >Activity</Title>
+          <Text
+            ef={contentRef}
+            animate={animation}
+            initial="hidden"
+            variants={{
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: .8, ease: [0.6, 0.05, -0.01, 0.9] },
+              },
+              hidden: { opacity: 0, y: 100 },
+            }}
+          >
+            <P>地域特性を最大限に活かす知恵と工夫をこらし</P>
+            <P>新しく構想し、生み出していく「創造」の時代に</P>
+            <P>立ち向かわなければなりません</P>
+          </Text>
+          <ActivityContainer
+            ef={contentRef}
+            animate={animation}
+            initial="hidden"
+            variants={{
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1.2, ease: [0.6, 0.05, -0.01, 0.9] },
+              },
+              hidden: { opacity: 0, y: 120 },
+            }}
+          >
             <Card>
               <ThumbnailContainer>
                 <Thumbnail></Thumbnail>
@@ -42,7 +88,11 @@ export const Activity = () => {
 
 export default Activity;
 
-const ActivityContainer = styled.div`
+const Section = styled(motion.div)`
+ height: 100%;
+`
+
+const ActivityContainer = styled(motion.div)`
   margin-top: 30px;
   display: flex;
   width: 600px;

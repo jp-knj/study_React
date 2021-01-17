9846fe7da2aaf8811a053dbb7989232d9f84b820
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-17 13:19:22 +0900

    :+1: Add News Section
    t

--- 12_framer-motion-website/src/components/homePage/News.js
+++ 12_framer-motion-website/src/components/homePage/News.js
@@ -1,11 +1,63 @@
 import React from 'react'
+import styled from "styled-components";
+import { Section } from './Hero'
+import { Wrapper, Title } from './About'
 
 export const News = () => {
   return (
     <>
-      <h1>News</h1>
+      <Section>
+        <Wrapper>
+          <Title>News</Title>
+          <Text>最新の情報を届けます。</Text>
+          <NewsList>
+            <Article>
+              <ArticleDate>2020.1.30</ArticleDate>
+              <ArticleContent>
+                <CategoryName>News</CategoryName>
+                <ArticleTitle>コロナ禍のため、イベント中止のお知らせ</ArticleTitle>
+              </ArticleContent>
+            </Article>
+            <Article>
+              <ArticleDate>2020.1.30</ArticleDate>
+              <ArticleContent>
+                <CategoryName>Events</CategoryName>
+                <ArticleTitle>コロナ禍のため、イベント中止のお知らせ</ArticleTitle>
+              </ArticleContent>
+            </Article>
+          </NewsList>
+        </Wrapper>
+      </Section>
     </>
   )
 }
 
 export default News;
+
+const Text = styled.p`
+  font-weight: 200;
+  letter-spacing: 5px;
+  mix-blend-mode: exclusion;
+  font-size: 20px;
+  margin-bottom: 20px;
+`
+const NewsList = styled.div``
+const Article = styled.div`
+  display: flex;
+  align-items: center;
+`
+const ArticleDate = styled.div``
+const ArticleContent = styled.div`
+  margin-left: 30px;
+  padding: 15px 0;
+  display: flex;
+`
+const CategoryName = styled.div`
+  padding: 5px 10px;
+  border: 1px solid black;
+  width: fit-content;
+`
+const ArticleTitle = styled.div`
+ font-size: 22px;
+ margin-left: 30px;
+`

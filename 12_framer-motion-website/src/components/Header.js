249d4d5f0d0a0fdcb261e958a3e535c3a7b721f9
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-17 14:52:00 +0900

    :art: Styles Header
    t

--- 12_framer-motion-website/src/components/Header.js
+++ 12_framer-motion-website/src/components/Header.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BiMenu } from "react-icons/bi";
 
 export const Header = () => {
   return (
     <>
-
+      <HeaderSection>
+        <HeaderWrapper>
+          <Menu><BiMenu size={32}/></Menu>
+        </HeaderWrapper>
+      </HeaderSection>
     </>
   )
 }
 
 export default Header
+
+const HeaderSection = styled.header`
+  width: 100%;
+`
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 0 auto;
+  height: 90px;
+  padding-right: 80px;
+  max-width: 1280px;
+`
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  height: 80px;
+  width: 80px;
+`

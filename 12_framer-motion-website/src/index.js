ef0f91fd56b72eb8675b95fc209c67a7a757d804
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-17 14:29:21 +0900

    :+1: Add Header and Footer
    t

--- 12_framer-motion-website/src/index.js
+++ 12_framer-motion-website/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Cursor from './components/Cursor';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Cursor from './components/Cursor'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cursor/>
+    <Cursor />
+    <Header/>
     <App />
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );

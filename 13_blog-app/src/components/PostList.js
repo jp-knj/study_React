a99b4e6b609a61d38f6627e1c3e316de558541c8
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-30 14:12:14 +0900

    :+1: Update src to dispatch some data
    t

--- 13_blog-app/src/components/PostList.js
+++ 13_blog-app/src/components/PostList.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { fetchPosts } from '../actions'
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
-)(PostList);;
+)(PostList);

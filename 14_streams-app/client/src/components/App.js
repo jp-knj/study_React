3e091af6f594c50b24f39f9484b40b1109905ed2
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-01-30 22:11:04 +0900

    :sparkles: Setup streams
    t

--- 14_streams-app/client/src/components/App.js
+++ 14_streams-app/client/src/components/App.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-};
-
-const PageTwo = () => {
-  return <div>PageTwo</div>;
-};
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Route path='/' exact component={PageOne} />
-        <Route path='/pagetwo' exact component={PageTwo} />
+        <>
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' component={StreamCreate} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
+        </>
       </BrowserRouter>
     </>
-
   )
 }
 

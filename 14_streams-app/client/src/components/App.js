8afeee5130cf85d67e40d7c27d5d7636c0f2b507
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-03 18:48:14 +0900

    :sparkles: Add Modal to delete each stream
    t

--- 14_streams-app/client/src/components/App.js
+++ 14_streams-app/client/src/components/App.js
@@ -15,9 +15,9 @@ const App = () => {
         <>
           <Header />
           <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new"  component={StreamCreate} />
+          <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete"  component={StreamDelete} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show"  component={StreamShow} />
         </>
       </Router>

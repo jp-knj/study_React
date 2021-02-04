a96a8748557f093bedb2a5d045a359b00f64d48f
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-04 20:03:38 +0900

    :+1: Connect the field and button to the store
    t

--- 15_translate-app/src/components/Button.js
+++ 15_translate-app/src/components/Button.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
-import LanguageContext from '../contexts/LanguageContext'
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
-  renderSubmit(value) {
-    return value === 'english' ? 'Submit' : 'Voorleggen';
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Voorleggen';
   }
 
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
     );
@@ -21,11 +20,10 @@ class Button extends React.Component {
   render() {
     return (
       <ColorContext.Consumer>
-        {(color) => this.renderButton(color)}
+        {color => this.renderButton(color)}
       </ColorContext.Consumer>
     );
   }
 }
-
 
 export default Button;

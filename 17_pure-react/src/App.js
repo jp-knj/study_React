e6b65f88f8b10de23310338d475e385495fa8007
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-07 02:36:58 +0900

    :+1: Update pretteir
    t

--- 17_pure-react/src/App.js
+++ 17_pure-react/src/App.js
@@ -1,29 +1,29 @@
-const Pet = ({name, animal, breed}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
+    React.createElement("h2", {}, breed),
   ]);
-}
+};
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Hello World"),
     React.createElement(Pet, {
       name: "たま",
       animal: "猫",
-      breed: "マタタビ"
+      breed: "マタタビ",
     }),
     React.createElement(Pet, {
       name: "ポチ",
       animal: "犬",
-      breed: "骨"
+      breed: "骨",
     }),
     React.createElement(Pet, {
       name: "ケンジ",
       animal: "ヒト",
-      breed: "酒"
+      breed: "酒",
     }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App),document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));

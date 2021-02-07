afa2cbe63c8e52b9f7b47a4f4d5ad6a1dc27ed08
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-07 21:58:22 +0900

    :+1: useState, custome hooks, update ESLint, bring in petfinder client
    t

--- 17_pure-react/src/App.js
+++ 17_pure-react/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Pet from './Pet';
+import SearchParams from "./SearchParams";
 
 const App = () => {
 
   return (
     <div>
       <h1>Hello World</h1>
-      <Pet name="タマ" animal="猫" breed="マタタビ" />
-      <Pet name="ポチ" animal="犬" breed="ドックフード" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams/>
     </div>
   );
 };

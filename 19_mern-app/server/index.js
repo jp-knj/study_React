e3d3471ee72a6e28b66c72562ddd02d224a7bf60
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-23 14:55:37 +0900

    :+1: Update index.js
    t

--- 19_mern-app/server/index.js
+++ 19_mern-app/server/index.js
@@ -1,29 +1,30 @@
 const dotenv = require('dotenv');
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const fileUplaod = require('express-fileupload');
-
-// Config
-dotenv.config();
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+const path = require('path')
 
 // Server
-const app = express();
-app.use(express.json());
-app.use(cookieParser);
-app.use(fileUplaod({
-  useTempFiles: true
-}));
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+// Config
+dotenv.config()
 
 // Middleware
-app.use(cors());
+app.use(cors())
 
 app.use('/', (req, res, next) => {
   res.json({ msg: "Hello Everyone!" })
-});
+})
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT)
+  console.log('Server is running on port', PORT)
 });

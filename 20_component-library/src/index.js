4111f049eb4f5fcce009d5bd34df821ba8a3a25e
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-24 16:33:23 +0900

    :+1: Add Global
    t

--- 20_component-library/src/index.js
+++ 20_component-library/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1>Hello World</h1>;
+import PrimaryButton, {
+  SecondaryButton,
+  TertiaryButton
+} from './components/Buttons';
+
+const App = () => (
+  <div>
+    <PrimaryButton>Hello World</PrimaryButton>
+    <SecondaryButton>Hello World</SecondaryButton>
+    <TertiaryButton>Hello World</TertiaryButton>
+  </div>
+)
 
 ReactDOM.render(<App />, document.querySelector("#root"));

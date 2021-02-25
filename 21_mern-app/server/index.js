bfc20a5300a71e0a4e8bd3c0b05b7b2ea6e44af0
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-25 10:30:47 +0900

    :sparkles: Add routes, controllers and config
    t

---
+++ 21_mern-app/server/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+const app = express()
+
+/// Config for development
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({
+    origin: process.env.CLIENT_URL
+  }))
+
+  app.use(morgan('dev'));
+  // Morgan give information about each request
+  // Cors allow to deal with react for localhost at port 3000 without any problem
+}
+
+// Config
+dotenv.config({ path: './config/config.env' })
+
+
+// Routes
+const authRouter = require('./routes/auth.route')
+app.use(`/api/`, authRouter)
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "PAGE NOT FOUND"
+  })
+})
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+})

4690e67633c75d940eb26c5872eb9e396f5a76a3
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-26 10:43:15 +0900

    :+1: Add Home page
    t

--- 21_mern-app/client/src/App.js
+++ 21_mern-app/client/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// Pages
+import Home from './pages/Home'
 import Register from './pages/Register'
+
+// CSS
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
-    <Register />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/register' exact component={Register}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
